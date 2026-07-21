8f6ac9f0394bb0e41cf939e19b0e4f92ecca0e3d
Dario Camonita <danterolle@parrotsec.org>
2026-07-21 15:43:59 +0200

    fix: simplify CLI startup banners

--- src/core/client/log.c
+++ src/core/client/log.c
@@ -3,35 +3,14 @@
 #include "version.h"
 
 void client_log_banner(const client_t *cli) {
-    char version[64], ip[64], server[64], tun[64], name[64], virt_ip[TUND_IP_STR_LEN],
-        server_ip[TUND_IP_STR_LEN];
-    snprintf(version, sizeof(version), "TunD Client v%s", TUND_VERSION);
-    snprintf(ip, sizeof(ip), "Virtual IP: %s",
-             ip_to_str_buf(cli->virt_ip, virt_ip, sizeof(virt_ip)));
-    snprintf(server, sizeof(server), "Server: %s:%u",
+    char virt_ip[TUND_IP_STR_LEN], server_ip[TUND_IP_STR_LEN];
+    LOG_INFO("TunD Client v%s", TUND_VERSION);
+    LOG_INFO("Virtual IP: %s", ip_to_str_buf(cli->virt_ip, virt_ip, sizeof(virt_ip)));
+    LOG_INFO("Server: %s:%u",
              ip_to_str_buf(cli->server_addr.sin_addr.s_addr, server_ip, sizeof(server_ip)),
              ntohs(cli->server_addr.sin_port));
-    snprintf(tun, sizeof(tun), "TUN: %s", cli->tun.ifname);
-    snprintf(name, sizeof(name), "Name: %s", cli->name);
-
-    int width = (int)strlen(version);
-    if ((int)strlen(ip) > width) width = (int)strlen(ip);
-    if ((int)strlen(server) > width) width = (int)strlen(server);
-    if ((int)strlen(tun) > width) width = (int)strlen(tun);
-    if ((int)strlen(name) > width) width = (int)strlen(name);
-
-    int inner = width + 4;
-    char line[128];
-    memset(line, '=', (size_t)inner);
-    line[inner] = '\0';
-
-    LOG_INFO("╔%s╗", line);
-    LOG_INFO("║  %-*s║", inner - 2, version);
-    LOG_INFO("║  %-*s║", inner - 2, ip);
-    LOG_INFO("║  %-*s║", inner - 2, server);
-    LOG_INFO("║  %-*s║", inner - 2, tun);
-    LOG_INFO("║  %-*s║", inner - 2, name);
-    LOG_INFO("╚%s╝", line);
+    LOG_INFO("TUN: %s", cli->tun.ifname);
+    LOG_INFO("Name: %s", cli->name);
     LOG_INFO("Press Ctrl+C to disconnect.");
 }
 
--- src/core/server/log.c
+++ src/core/server/log.c
@@ -3,32 +3,12 @@
 #include "version.h"
 
 void server_log_banner(const server_t *srv) {
-    char version[64], port[64], ip[64], tun[64], server_ip[TUND_IP_STR_LEN];
-    snprintf(version, sizeof(version), "TunD Server v%s", TUND_VERSION);
-    snprintf(port, sizeof(port), "Listening on UDP port %u", srv->port);
-    snprintf(ip, sizeof(ip), "Virtual IP: %s",
-             ip_to_str_buf(htonl(TUND_SERVER_IP), server_ip, sizeof(server_ip)));
-    snprintf(tun, sizeof(tun), "TUN: %s", srv->tun.ifname);
-    char subnet[] = "Subnet: 10.9.0.0/24";
-
-    int width = (int)strlen(version);
-    if ((int)strlen(port) > width) width = (int)strlen(port);
-    if ((int)strlen(ip) > width) width = (int)strlen(ip);
-    if ((int)strlen(subnet) > width) width = (int)strlen(subnet);
-    if ((int)strlen(tun) > width) width = (int)strlen(tun);
-
-    int inner = width + 4;
-    char line[128];
-    memset(line, '=', (size_t)inner);
-    line[inner] = '\0';
-
-    LOG_INFO("╔%s╗", line);
-    LOG_INFO("║  %-*s║", inner - 2, version);
-    LOG_INFO("║  %-*s║", inner - 2, port);
-    LOG_INFO("║  %-*s║", inner - 2, ip);
-    LOG_INFO("║  %-*s║", inner - 2, subnet);
-    LOG_INFO("║  %-*s║", inner - 2, tun);
-    LOG_INFO("╚%s╝", line);
+    char server_ip[TUND_IP_STR_LEN];
+    LOG_INFO("TunD Server v%s", TUND_VERSION);
+    LOG_INFO("Listening on UDP port %u", srv->port);
+    LOG_INFO("Virtual IP: %s", ip_to_str_buf(htonl(TUND_SERVER_IP), server_ip, sizeof(server_ip)));
+    LOG_INFO("Subnet: 10.9.0.0/24");
+    LOG_INFO("TUN: %s", srv->tun.ifname);
 }
 
 void server_log_startup_checklist(const server_t *srv) {
